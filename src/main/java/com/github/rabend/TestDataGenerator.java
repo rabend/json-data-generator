184d40ef0f7e33b0cc19f674ffd263043988e722
Roman Abendroth <r.abendroth@tarent.de>
2020-03-06 09:32:30 +0100

    add java doc

--- src/main/java/com/github/rabend/TestDataGenerator.java
+++ src/main/java/com/github/rabend/TestDataGenerator.java
@@ -12,11 +12,21 @@ public class TestDataGenerator {
     private final URL schemaUrl;
     private final ObjectMapper mapper;
 
+    /**
+     * Constructor
+     * @param schemaUrl URL to a valid json schema
+     */
     public TestDataGenerator(final URL schemaUrl) {
         this.schemaUrl = schemaUrl;
         this.mapper = new ObjectMapper();
     }
 
+    /**
+     * Generates a json string with random values for the keys defined in the json schema.
+     * Values will be generated according to any restrictions in the schema, if present.
+     * @return a json string
+     * @throws IOException
+     */
     public String generateJsonString() throws IOException {
         JsonNode baseNode = mapper.readTree(this.schemaUrl);
         return new ObjectGenerator().generateRandomValue(baseNode);
