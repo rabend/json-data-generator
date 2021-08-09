2312ebb70681095775447693d32d0740d24d34d0
Roman Abendroth <roman.abendroth@rewe-digital.com>
2021-08-09 17:24:56 +0200

    migrate StringGenerator.kt and tests

--- src/main/java/com/github/rabend/generators/StringGenerator.kt
+++ src/main/java/com/github/rabend/generators/StringGenerator.kt
@@ -1,41 +1,47 @@
 package com.github.rabend.generators
 
-import com.fasterxml.jackson.databind.JsonNode
 import com.mifmif.common.regex.Generex
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.JsonPrimitive
+import kotlinx.serialization.json.int
+import kotlinx.serialization.json.jsonArray
+import kotlinx.serialization.json.jsonPrimitive
 import java.util.*
 import java.util.concurrent.ThreadLocalRandom
 
 class StringGenerator : AbstractValueGenerator() {
-    override fun generateRandomValue(node: JsonNode?): String? {
-        if (node!!.has(FORMAT)) {
-            if (node[FORMAT].asText().equals("date", ignoreCase = true)) {
-                return "\"" + SimpleDateGenerator().generateDate().toString() + "\""
-            } else if (node[FORMAT].asText().equals("date-time", ignoreCase = true)) {
-                return "\"" + SimpleDateGenerator().generateDateTime().toString() + "\""
+    override fun generateRandomValue(node: JsonObject): JsonElement {
+        if (node.containsKey(FORMAT)) {
+            val format = node[FORMAT]!!.jsonPrimitive.content
+            if (format.equals("date", ignoreCase = true)) {
+                return JsonPrimitive(SimpleDateGenerator().generateDate().toString())
+            } else if (format.equals("date-time", ignoreCase = true)) {
+                return JsonPrimitive(SimpleDateGenerator().generateDateTime().toString())
             }
         }
         var maxLength = 50
         var minLength = 6
-        if (node.has("maxLength")) {
-            maxLength = node["maxLength"].asInt() + 1
+        if (node.containsKey("maxLength")) {
+            maxLength = node["maxLength"]!!.jsonPrimitive.int + 1
             minLength = 0
         }
-        if (node.has("minLength")) {
-            minLength = node["minLength"].asInt()
+        if (node.containsKey("minLength")) {
+            minLength = node["minLength"]!!.jsonPrimitive.int
         }
-        if (node.has("pattern")) {
-            var regex = node["pattern"].asText()
+        if (node.containsKey("pattern")) {
+            var regex = node["pattern"]!!.jsonPrimitive.content
             regex = regex.replace("^", "")
             regex = regex.replace("$", "")
             val generex = Generex(regex)
-            return "\"" + generex.random(minLength, maxLength - 1) + "\""
+            return JsonPrimitive(generex.random(minLength, maxLength - 1))
         }
         val random = ThreadLocalRandom.current()
-        if (node.has("enum")) {
-            val enumNode = node.withArray<JsonNode>("enum")
-            val bound = if (enumNode.size() == 1) 1 else enumNode.size() - 1
-            val randomIndex = random.nextInt(bound)
-            return "\"" + enumNode[randomIndex].asText() + "\""
+        if (node.containsKey("enum")) {
+            val enumNode = node["enum"]!!.jsonArray
+            val upperBound = if (enumNode.size == 1) 1 else enumNode.size - 1
+            val randomIndex = random.nextInt(upperBound)
+            return JsonPrimitive(enumNode[randomIndex].jsonPrimitive.content)
         }
         val randomLength = random.nextInt(minLength, maxLength)
         val chars = CharArray(randomLength)
@@ -43,7 +49,8 @@ class StringGenerator : AbstractValueGenerator() {
             val bound = random.nextInt(ALL_CHARS.length - 1)
             chars[i] = ALL_CHARS.toCharArray()[bound]
         }
-        return "\"" + String(chars) + "\""
+
+        return JsonPrimitive(String(chars))
     }
 
     companion object {
